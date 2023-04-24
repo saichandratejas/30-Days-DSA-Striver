31c6d4129a02c569cbb554c01cfab898a622187b
sai_tejas <saitejassharma@gmail.com>
2023-04-24 23:42:52 +0530

    Create armstrong_no.cpp

---
+++ gfg/armstrong_no.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B;
+    cout << "Enter two numbers";
+    cin >>A>>B;
+    int lcm;
+    for(int i=1;i>0; i++)
+    {
+        if(i%A==0 && i%B==0){
+            lcm=i;
+            cout << "The lcm is " <<i;
+            break;
+        }
+    }
+    return 0;
+};
+/*#include<iostream>
+using 
